ce5496995d33a06bb181c30f501b9f413032c8fb
lawrenceweed <lawrenceweed@hotmail.co.uk>
2024-09-24 08:44:01 +0100

    Fix Logo 3

--- headerFooterManager.js
+++ headerFooterManager.js
@@ -1,7 +1,7 @@
 class specialHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML =
-            '<nav class="navbar"><div class="container"><div class="logo"><a href="home.html"><img src="images/Logo (No Name) with White.png" alt="logo" width="80px"><span class="logo-name"><font color="#FFFFF">HSH</font> <font color="#0171BB">Productions</font></span></a></div></div></nav >';
+            '<nav class="navbar"><div class="container"><div class="logo"><a href="home.html"><img src="images/Logo (No Name) with White.png" alt="logo" width="80px"><span class="logo-name"><font color="#FFFFF">HSH</font> <font color="#0171BB">Productions</font></span></a></div></div></nav>';
     }
 }
 
